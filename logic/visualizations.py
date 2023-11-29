221d87ebf1847e721d1c7caf8c6d2b36763f22d5
glad2os <lite.console4bit@gmail.com>
2023-11-29 13:24:23 -0500

    fix visualizations.py

--- logic/visualizations.py
+++ logic/visualizations.py
@@ -3,13 +3,16 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import io
 
-matplotlib.use('Agg')  # Use a non-interactive backend
+matplotlib.use('Agg')
 
 
 def create_plot(data):
+    year_counts = data['OCC_YEAR'].value_counts().reset_index()
+    year_counts.columns = ['OCC_YEAR', 'count']
+    threshold = 10
+    filtered_year_counts = year_counts[year_counts['count'] >= threshold]
     plt.figure(figsize=(10, 6))
-    sns.countplot(x='OCC_YEAR', data=data)
-    # Generate the plot
+    sns.barplot(x='OCC_YEAR', y='count', data=filtered_year_counts)
     buf = io.BytesIO()
     plt.savefig(buf, format='png')
     buf.seek(0)
